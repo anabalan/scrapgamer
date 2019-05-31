a44f16db5744a5a6714344566aa0ca8fef83be39
Ana <anabalan.b@gmail.com>
2019-05-31 11:06:35 +0000

    32 - atualizando função do site 2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,57 @@
 // pega o cors e taca longe, aqui a gente passa por ele
 let link = 'https://cors-anywhere.herokuapp.com/'
 
+
+// aqui limpamos o conteúdo da div quando o botão for clicado, e adicionamos o conteúdo
+var func1Executada = false;
+var func2Executada = false;
+var func3Executada = false;
+
+Array.from($(".search").children('button')).forEach(function(element) {
+    element.addEventListener("click", function(evt) {
+        var valorTab = $(this).attr('id');
+
+
+        if(valorTab === 'site1'){
+            if($('.section-reviews').children().length > 0){
+                var reviews = $('.section-reviews').children();
+                $(reviews, '.hr').remove()
+                func1Executada = true;
+                if(func1Executada){
+                    return false;
+                }
+                site1();
+            }else{
+                return false;
+            }
+        } else if(valorTab === 'site2'){
+            if($('.section-reviews').children().length > 0){
+                var reviews = $('.section-reviews').children();
+                $(reviews, '.hr').remove()
+                func2Executada = true;
+                if(func2Executada){
+                    return false;
+                }
+                site2();
+            }else{
+                return false;
+            }
+        } else if(valorTab === 'site3'){
+            if($('.section-reviews').children().length > 0){
+                var reviews = $('.section-reviews').children();
+                $(reviews, '.hr').remove()
+                func3Executada = true;
+                if(func3Executada){
+                    return false;
+                }
+                site3();
+            }else{
+                return false;
+            }
+        }         
+    })
+});
+
 // função para pegar o conteúdo do site 1 - The Enemy
 function site1() {
     $.ajax({
@@ -25,15 +76,18 @@ function site1() {
             noticias.forEach(function(tagHtml) {
                 //  Raspagem com js purinho
                 let imagens = $(tagHtml).children('').children().attr('src');
-                // tirando o // da url da imagem
+                // tirando o // da url da imagem "tentativa falha"
                 let imagemLimpa = imagens.replace('//', '');
-                // console.log(imagemLimpa);
+                console.log(imagemLimpa);
+                
+                
                 // Jogando o conteúdo nas variáveis
                 let titulo = $(tagHtml).children().children().children('.news-list__item__content__title').text();
                 let descricao = $(tagHtml).children().children().children('.news-list__item__content__description').text();
+
                 // console.log(descricao);
 
-            // jogando o conteúdo nas div
+
             let divreview = `
             <div class="review">
             <div class="img-review" style="background-image: url(https://${imagemLimpa});"></div>
@@ -44,6 +98,7 @@ function site1() {
             </div>
             <hr/>
               `;
+            //   console.log(divreview);
                 $('.section-reviews').append(divreview)
             });
         }
@@ -52,10 +107,11 @@ function site1() {
     });
 }
 
-// função ao clicar no botão do filtro
 $("#site1").on("click",function(){
    site1()
 });
+
+
 
 
 // função para pegar o conteúdo do site 2 - Voxel
@@ -75,17 +131,16 @@ function site2() {
 
             // selecionando a div que contém a notícia
             var noticias = documento.body.querySelectorAll('.home-vertical-news-item');
-            
+
             // console.log(noticias);
-            
+
             // loop por cada div de notícia existente nessa birosca de site
             noticias.forEach(function(tagHtml) {
                 //  Raspagem com js purinho
                 let imagens = $(tagHtml).children().children('.home-vertical-news-img .lazyloaded').attr('src');
                 // Jogando o conteúdo nas variáveis
                 let titulo = $(tagHtml).children().children().children('.home-vertical-news-title').text();
-            
-            // jogando o conteúdo nas divs
+
             let divreview = `
             <div class="review">
             <div class="img-review" style="background-image: url(https://${imagens});"></div>
@@ -96,6 +151,7 @@ function site2() {
             </div>
             <hr/>
               `;
+            //   console.log(divreview);
                 $('.section-reviews').append(divreview)
             });
         }
@@ -107,6 +163,9 @@ function site2() {
 $("#site2").on("click",function(){
    site2()
 });
+
+
+
 
 // função para pegar o conteúdo do site 3
 function site3() {
@@ -169,45 +228,7 @@ $(document).ready(function(){
 });
 
 
-// fazendo o "filtro"
-var func1Executada = false;
-var func2Executada = false;
-var func3Executada = false;
-
-Array.from($(".search").children('button')).forEach(function(element) {
-    element.addEventListener("click", function(evt) {
-        var valorTab = $(this).attr('id');
-
-
-        if(valorTab === 'site1'){
-            if($('.section-reviews').children().length > 0){
-                var reviews = $('.section-reviews').children();
-                $(reviews, '.hr').remove()
-                func1Executada = true;
-                if(func1Executada){
-                    return false;
-                }
-                site1();
-            }else{
-                return false;
-            }
-        } else if(valorTab === 'site2'){
-            if($('.section-reviews').children().length > 0){
-                var reviews = $('.section-reviews').children();
-                $(reviews, '.hr').remove()
-                func2Executada = true;
-                if(func2Executada){
-                    return false;
-                }
-                site2();
-            }else{
-                return false;
-            }
-        }    
-    })
-});
-
-//Efeito no botão ativo
+// Efeio no botão ativo
 // $(document).ready(function(){
 //     $("button").click(function(){
 //         $("button").removeClass("fundo-btn");
